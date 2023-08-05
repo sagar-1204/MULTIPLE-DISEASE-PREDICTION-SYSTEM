49fd3a88998e22297b26b1711acc393538816897
Sagar Reddy R <127428321+sagar-1204@users.noreply.github.com>
2023-08-05 16:12:35 +0530

    multiple disease pred.py

--- Multiple Disease Prediction System/multiple disease pred.py
+++ Multiple Disease Prediction System/multiple disease pred.py
@@ -14,9 +14,9 @@ from streamlit_option_menu import option_menu
 
 diabetes_model = pickle.load(open('C:/Users/sagar/Desktop/Multiple Disease Prediction System/saved models/diabetes_model.sav', 'rb'))
 
-heart_disease_model = pickle.load(open('C:/Users/siddhardhan/Desktop/Multiple Disease Prediction System/saved models/heart_disease_model.sav','rb'))
+heart_disease_model = pickle.load(open('C:/Users/sagar/Desktop/Multiple Disease Prediction System/saved models/heart_disease_model.sav','rb'))
 
-parkinsons_model = pickle.load(open('C:/Users/siddhardhan/Desktop/Multiple Disease Prediction System/saved models/parkinsons_model.sav', 'rb'))
+parkinsons_model = pickle.load(open('C:/Users/sagar/Desktop/Multiple Disease Prediction System/saved models/parkinsons_model.sav', 'rb'))
 
 
 
